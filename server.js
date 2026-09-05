9dd9a4a263a539a1354915fbf571fad676cdf6f2
yeom-heesu <2016gmltn@naver.com>
2026-09-05 15:23:54 +0900

    UI 수정

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.session.user || null;
   res.locals.currentPath = req.path;
   res.locals.isAdmin = isAdmin(req.session.user);
+  res.locals.guildIntro = guildIntro;
   next();
 });
 
